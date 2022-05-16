ea978ca00e2c754b61f394d77429525cf0f259f6
Oleg <olegkurbatov97@mail.ru>
2022-05-16 21:15:45 +0600

    base

---
+++ src/components/Payment.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="payment">
+    <div class="payment__contact">
+      <div>Select contact</div>
+      <div class="contact__select"></div>
+    </div>
+    <div class="payment__method">
+      <div>Select contact</div>
+      <div class="method__select"></div>
+    </div>
+    <div class="payment__amount">
+      <div class="text-3xl font-bold underline">Select contact</div>
+      <div class="amount__value"></div>
+    </div>
+    <div>
+      <button class="btn bg-purple-900 text-white">Pay</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from "vue-router";
 import App from './App.vue'
+import './index.css'
 
-createApp(App).mount('#app')
+import Payment from './components/Payment.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Payment', component: Payment},
+    ]
+})
+
+createApp(App).use(router).mount('#app')
